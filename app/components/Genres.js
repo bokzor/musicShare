74df296db343b34b1d1c17c01d0c53c1e2d62e18
Adrien Bokor <adrien@bokor.be>
2015-10-12 16:43:04 +0200

    add genres list

--- app/components/Genres.js
+++ app/components/Genres.js
@@ -1,17 +1,9 @@
 import React from 'react';
 
 class Genre extends React.Component {
-    componentDidMount() {
-        // Get the components DOM node
-        var elem = React.findDOMNode(this);
-        // Set the opacity of the element to 0
-        elem.style.opacity = 0;
-        window.requestAnimationFrame(function() {
-            // Now set a transition on the opacity
-            elem.style.transition = "opacity 250ms";
-            // and set the opacity to 1
-            elem.style.opacity = 1;
-        });
+    constructor(props) {
+        super(props);
+        this.state = {count: props.initialCount};
     }
     render () {
         return(
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    database: process.env.MONGO_URI || 'mongodb://music:aaaaaaa1@ds031631.mongolab.com:31631/musicshare'
+    database: process.env.MONGO_URI || 'mongodb://music:aaaaaaa1@ds031631.mongolab.com:31631/musicshare',
+    genres: ['Deep House', 'Drum & Bass', 'Dubstep', 'EDM', 'Electronic',  'Techno', 'Trance', 'Tech-house']
 };
